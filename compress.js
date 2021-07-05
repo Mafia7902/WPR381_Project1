0d0c657c42ab50829f8cbba950a781184dd56c83
LukeJuerg <576601@student.belgiumcampus.ac.za>
2021-07-05 11:29:37 +0200

    added functions for each file type

--- compress.js
+++ compress.js
@@ -1,25 +1,27 @@
 var compress = require('compressing');
 
-var choice = "tar";
+var choice = "zip";
 
 switch (choice) {
     case "zip":
         console.log("this is zip")
+
+        compress.zip.compressFile('F:\\Users\\Luke\\Desktop\\CompressThis.txt', 'F:\\Users\\Luke\\Desktop\\CompressThis.zip').then(console.log("File has been sucessfully compressed to .zip")).catch(console.log("Error!"));
         break;
 
     case "tar":
         console.log("this is tar")
-        break;
 
-    case "gzip":
-        console.log("this is gzip")
+        compress.tar.compressFile('', '').then(console.log("File has been sucessfully compressed to .tar")).catch(console.log("Error!"));
         break;
 
     case "tgz":
+
+        compress.tgz.compressFile('', '').then(console.log("File has been sucessfully compressed to .tgz")).catch(console.log("Error!"));
         console.log("this is tgz")
         break;
 
     default:
-        console.log("Bullshit")
+        console.log("Error! File extension not supported!")
         break;
 }
