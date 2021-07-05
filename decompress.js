e5127ad75cfb5f5cb42a9f3d55fb8d1646d6840a
Stef98 <stefferreira14@gmail.com>
2021-07-05 16:33:08 +0200

    Finished decompression

--- decompress.js
+++ decompress.js
@@ -1,28 +1,34 @@
-var compressing = require('compressing');
+var compressing = require("compressing");
 var choice = "";
-var poes = "C:\\decompressed.zip";
-var type = poes.substring(poes.length-3,poes.length);
+
+var path = "C:\\decompressed.zip";
+
+var type = path.substring(path.length - 3, path.length);
 console.log(type);
- switch (choice){
-   case "zip":
-     var type = poes.substring(".", 3);
-     compressing.zip.uncompress('C:\\decompressed.zip', 'C:\\decompressed.zip')
-     .then(console.log("the file has been decompressed"))
-     .catch(console.log("This file has not been decompressed"));
-  break;   
- case "tar":
- compressing.tar.uncompress('C:\\decompressed.tar', 'C:\\decompressed.tar')
- .then(uncompressDone)
- .catch(handleError);
- break;
 
- case "tgz":
+switch (choice) {
+  case "zip":
+    //var type = path.substring(".", 3);
+    compressing.zip
+      .uncompress("C:\\decompressed.zip", "C:\\decompressed.zip")
+      .then(console.log("the file has been decompressed"))
+      .catch(console.log("This file has not been decompressed"));
+    break;
 
- break;
+  case "tar":
+    compressing.tar
+      .uncompress("C:\\decompressed.tar", "C:\\decompressed.tar")
+      .then(console.log("The file has been decompressed"))
+      .catch(console.log("This file has not been decompressed"));
+    break;
 
- default:
- console.log("IDK");
+  case "tgz":
+    compressing.tgz
+      .uncompress("C:\\decompressed.tgz", "C:\\decompressed.tgz")
+      .then(console.log("The file has been decompressed"))
+      .catch(console.log("This file has not been decompressed"));
+    break;
 
-
+  default:
+    console.log("IDK");
 }
-
